c0bde072692de9ddcdb134d611b264a18634df55
SonyaMoisset <sonya.moisset@gmail.com>
2016-02-01 19:25:07 +0900

    Add flashcardSite

---
+++ introToProgramming/JSandjQuery/flashcardSite/scripts.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+  $(".JS").click(function() {
+    $(".defJS").toggle();
+  });
+
+  $(".modulo").click(function() {
+    $(".defModulo").toggle();
+  });
+
+  $(".operators").click(function() {
+    $(".defOperators").toggle();
+  });
+
+  $(".nan").click(function() {
+    $(".defNan").toggle();
+  });
+
+  $(".vnc").click(function() {
+    $(".defVnc").toggle();
+  });
+
+  $(".func").click(function() {
+    $(".defFunc").toggle();
+  });
+});
