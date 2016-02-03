d53f70b65b9e5586dc187f10a78f423691afee75
SonyaMoisset <sonya.moisset@gmail.com>
2016-02-03 15:52:35 +0900

    Update specs.js

--- introToProgramming/BDD/leap-year/spec/specs.js
+++ introToProgramming/BDD/leap-year/spec/specs.js
@@ -17,9 +17,5 @@ describe('leapYear', function() {
 });
 
 var leapYear = function(year) {
-  if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
-    return true;
-  } else {
-    return false;
-  }
+  return ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0);
 };
