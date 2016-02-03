629cee02df31a1b0298f678ac3a3833180fb9ba4
SonyaMoisset <sonya.moisset@gmail.com>
2016-02-03 15:48:33 +0900

    Update specs.js

--- introToProgramming/BDD/leap-year/spec/specs.js
+++ introToProgramming/BDD/leap-year/spec/specs.js
@@ -6,10 +6,16 @@ describe('leapYear', function() {
   it("is true for years divisible by 4", function() {
     expect(leapYear(2004)).to.equal(true);
   });
+
+  it("is false for years divisible by 100", function() {
+    expect(leapYear(1900)).to.equal(false);
+  });
 });
 
 var leapYear = function(year) {
-  if (year % 4 === 0) {
+  if (year % 100 === 0) {
+    return false;
+  } else if (year % 4 === 0) {
     return true;
   } else {
     return false;
