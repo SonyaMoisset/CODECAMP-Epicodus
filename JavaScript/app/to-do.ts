d7983b692f6bc07453b0439ed3addd1140cb040b
SonyaMoisset <sonya.moisset@gmail.com>
2016-03-27 22:31:00 +0900

    Update to-do files

--- JavaScript/app/to-do.ts
+++ JavaScript/app/to-do.ts
@@ -1,14 +1,29 @@
-class Task {
+interface IPerson {
+  name: string;
+  email: string;
+}
+
+interface ITask {
+  description: string;
+  done: boolean;
+  priority: string;
+  markDone(): void;
+  assignedTo?: IPerson;
+}
+
+class Task implements ITask {
   done: boolean = false;
-
-  constructor( public description: string, public priority: string ) {}
-
+  constructor( public description: string, public priority: string, public assignedTo?: IPerson ) {}
   markDone() {
     this.done = true;
   }
 }
 
-class HomeTask extends Task {}
+class HomeTask extends Task {
+  constructor( public description: string, public priority: string, public assignedTo?: IPerson ) {
+    super( description, priority );
+  }
+}
 
 class HobbyTask extends Task {
   constructor( public description: string ) {
@@ -17,14 +32,29 @@ class HobbyTask extends Task {
 }
 
 class WorkTask extends Task {
-  constructor( public dueDate: Date, public description: string, public priority: string ) {
-    super( description, priority );
+  constructor( public dueDate: Date, public description: string, public priority: string, public assignedTo: IPerson ) {
+    super( description, priority, assignedTo );
   }
+}
+
+var diane: IPerson = {
+  name: "Diane D",
+  email: "diane@epicodus.com"
+}
+
+var thor: IPerson = {
+  name: "Thor Son of Odin",
+  email: "thor@asgard.com"
+}
+
+var loki: IPerson = {
+  name: "God of mischief",
+  email: "loki@geocities.com"
 }
 
 var tasks = [];
 tasks.push( new HomeTask( "Do the dishes.", "High" ) );
-tasks.push( new HomeTask( "Buy chocolate.", "Low" ) );
+tasks.push( new HomeTask( "Buy chocolate.", "Low", diane ) );
 tasks.push( new HomeTask( "Wash the laundry.", "High" ) );
 
 tasks.push( new HobbyTask("Practice origimai." ) );
@@ -36,8 +66,8 @@ tomorrow.setDate( today.getDate() + 1 );
 var nextDay = new Date();
 nextDay.setDate( today.getDate() + 2 );
 
-tasks.push( new WorkTask( today, "Update blog.", "High" ) );
-tasks.push( new WorkTask( tomorrow, "Go to meeting.", "Medium" ) )
-tasks.push( new WorkTask( nextDay, "Clean ceiling.", "Low" ) )
+tasks.push( new WorkTask( today, "Update blog.", "High", diane ) );
+tasks.push( new WorkTask( tomorrow, "Go to meeting.", "Medium", thor ) )
+tasks.push( new WorkTask( nextDay, "Clean ceiling.", "Low", loki ) )
 
 console.log(tasks);
